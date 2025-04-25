88c9b6d87605f76b4c3bdc4c1d01598c0c0b0462
Fema19 <fachrinurulakbar19@gmail.com>
2025-04-25 08:11:53 +0700

    check

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\EkskulController;
 use Illuminate\Support\Facades\Route;
 
+// Halaman utama tetap welcome
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
+})->name('dashboard');
+
+// Ganti ini agar tidak langsung return view, tapi pakai controller
+Route::get('/ekskul', function () {
+    return view('ekskul/index');
 });
+
+// Gunakan route resource agar semua fitur ekskul aktif
+Route::resource('ekskul', EkskulController::class);
